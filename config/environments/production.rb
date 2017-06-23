0782aac21ea215b3db9544bf15681c27e96cfd9e
Daniel Byrne <byrne.daniel87@gmail.com>
2017-06-23 11:33:05 +0100

    add mailer to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,6 +10,23 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
+
+  config.mailer_sender = 'byrne.daniel87@gmail.com'
+
+  config.action_mailer.default_url_options = { :host => 'wikisystem.herokuapp.com' }
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+  :address      => 'smtp.sendgrid.net',
+  :port         => '587',
+  :authentication   => :plain,
+  :user_name      => ENV["SENDGRID_USERNAME"],
+  :password       => ENV["SENDGRID_PASSWORD"],
+  :domain       => 'heroku.com',
+  :enable_starttls_auto => true
+  }
+
+
+
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
