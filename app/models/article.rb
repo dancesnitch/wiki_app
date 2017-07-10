e7c4333f2ab052f8714233eb72e2d911b3a2ed55
Daniel Byrne <byrne.daniel87@gmail.com>
2017-07-10 15:19:36 -0400

    add search to development

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,4 +5,8 @@ class Article < ApplicationRecord
 	def self.search(search)
   	where("title::text LIKE ? OR category_id::text LIKE ? OR content::text LIKE ?", "%#{search}%", "%#{search}%", "%#{search}%") 
 	end
+	#Added after production for development only
+	def self.search(search)
+  	where("title LIKE ? OR category_id LIKE ? OR content LIKE ?", "%#{search}%", "%#{search}%", "%#{search}%") 
+	end
 end
