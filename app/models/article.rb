bfca4a44449c1b95357eaaaad6fa0bc108f37fca
Daniel Byrne <byrne.daniel87@gmail.com>
2017-07-10 12:55:48 -0400

    testing search in localhost

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,6 +3,6 @@ class Article < ApplicationRecord
 	belongs_to :category
 
 	def self.search(search)
-  	where("title::text LIKE ? OR category_id::text LIKE ? OR content::text LIKE?", "%#{search}%", "%#{search}%", "%#{search}%") 
+  	where("title::text LIKE ? OR category_id::text LIKE ? OR content::text LIKE ?", "%#{search}%", "%#{search}%", "%#{search}%") 
 	end
 end
